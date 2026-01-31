29816b20f0f902317750231a214bd27e686911b3
zhangjx0810 <32645234+zhangjx0810@users.noreply.github.com>
2026-01-31 15:39:30 +0800

    Create run_extract_asnwer.sh

---
+++ extracting/extract_answer/run_extract_asnwer.sh
@@ -0,0 +1,4 @@
+python extract_label.py \
+  --input_json /export/home2/jianxiong/contrast/mot/step2/QTA/Qwen3/math/new/exp2_Qwen3-8B_resume_math500_EVA_Qwen3-32B_Qwen3_8B_new.json \
+  --processed_json /export/home2/jianxiong/contrast/ICM/extracting/label/new/result/truthfulqa/label.json \
+  --output_npy /export/home2/jianxiong/contrast/ICM/extracting/label/new/result/truthfulqa
