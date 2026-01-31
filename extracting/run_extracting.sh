8797adc40da9a72212d564a34d334c469a5fcf1a
zhangjx0810 <32645234+zhangjx0810@users.noreply.github.com>
2026-01-31 15:27:05 +0800

    Create run_extracting.sh

---
+++ extracting/run_extracting.sh
@@ -0,0 +1,6 @@
+python extract_embeddings.py \
+    --data_file /export/home2/jianxiong/contrast/ICM/vargenerate/new/result/Qwen_Qwen3-8B_exp1_Qwen3-8B_truthful_qa_noisy.json \
+    --model_name Qwen/Qwen3-8B \
+    --save_dir /export/home2/jianxiong/contrast/ICM/extracting/new/result/truthfulqa \
+    --batch_size 1 \
+    --seed 42
