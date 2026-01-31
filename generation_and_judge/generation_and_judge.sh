99882964532464f6858e92e66cf5716cdc277e58
zhangjx0810 <32645234+zhangjx0810@users.noreply.github.com>
2026-01-31 15:08:09 +0800

    Create generation_and_judge.sh

---
+++ generation_and_judge/generation_and_judge.sh
@@ -0,0 +1,8 @@
+python /export/home2/jianxiong/contrast/mot/new/generation_and_judge.py \
+    --model_name deepseek-ai/DeepSeek-R1-Distill-Llama-8B \
+    --dataset_name truthful_qa \
+    --gen_mode greedy \
+    --run_judge \
+    --judge_model Qwen/Qwen3-32B \
+    --max_samples 100 \
+    --num_samples 5
